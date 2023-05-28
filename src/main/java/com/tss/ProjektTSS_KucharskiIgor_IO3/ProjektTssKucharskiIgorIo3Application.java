752805105b52bb22dab2b50ad5c2a8f38ababc62
ikucharski <igorkucharski64@gmail.com>
2023-05-28 13:35:59 +0200

    Built project with dependencies

---
+++ src/main/java/com/tss/ProjektTSS_KucharskiIgor_IO3/ProjektTssKucharskiIgorIo3Application.java
@@ -0,0 +1,13 @@
+package com.tss.ProjektTSS_KucharskiIgor_IO3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektTssKucharskiIgorIo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektTssKucharskiIgorIo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tss/ProjektTSS_KucharskiIgor_IO3/ProjektTssKucharskiIgorIo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.ProjektTSS_KucharskiIgor_IO3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektTssKucharskiIgorIo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
